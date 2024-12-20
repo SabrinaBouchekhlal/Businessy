c7062ee575590d478edd55215f82febd888f0648
Zanoun Nada <nada.zanoun@ensia.edu.dz>
2024-12-20 10:48:13 +0100

    made models, events and states for the main features

--- lib/blocs/insights/insights_event.dart
+++ lib/blocs/insights/insights_event.dart
@@ -2,16 +2,10 @@ import 'package:equatable/equatable.dart';
 
 abstract class InsightsEvent extends Equatable {
   const InsightsEvent();
+
   @override
   List<Object> get props => [];
 }
+class LoadWeeklySalesData extends InsightsEvent {}
 
-class LoadFinancialOverviewEvent extends InsightsEvent {}
-
-class LoadTopSoldItemsEvent extends InsightsEvent {
-  final DateTime period;
-
-  const LoadTopSoldItemsEvent({required this.period});
-  @override
-  List<Object> get props => [period];
-}
+class LoadTopSoldItems extends InsightsEvent {}
